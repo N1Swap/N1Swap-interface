ebcf0bb1182f48dab01aaaa6f8525647ab87e470
blackrabbit <rabbitblack944@gmail.com>
2021-04-13 16:14:20 +0800

    update production config

--- n1swap/config/production.js
+++ n1swap/config/production.js
@@ -1,10 +1,9 @@
 module.exports =  {
     publicRuntimeConfig:  {
         'env' :  {
-            'API'               : 'http://api.n1swap.com',
+            'API'               : 'https://api.n1swap.com',
             'TRONSCAN_URL'      : 'https://shasta.tronscan.org',
             'WEBSITE'           : 'https://www.pixelschain.com',
-            'LOGO'              : 'test',
             'CONTRACT_ADDRESS'  : 'TJPUJWATUCDwfTZrqWxqUqaio7dSht3Sy1',
             'RECAPTCHA_V2_KEY'  : '6LdvIqEaAAAAAGLBf9ikxM5CnbCXamjVu6_fTuj9'
         },
